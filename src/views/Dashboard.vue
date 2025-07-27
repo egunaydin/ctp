2baf812a2e826c002a31378f1d55eebb3ec15a4e
Ensar <dev.ensargunaydin@gmail.com>
2025-07-27 18:49:10 +0300

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container py-4">
-    <h1 class="mb-4">Dashboard</h1>
+    <h1 class="mb-4">Dashboard1</h1>
     <p>This is your logistics tracking dashboard.</p>
   </div>
 </template>
